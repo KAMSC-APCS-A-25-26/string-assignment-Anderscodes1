74ca9e75251234c056a5d630df578baa2a5bca69
Anderscodes1 <anderschasegustafson@icloud.com>
2025-11-10 17:42:30 -0500

    Update WordCensor.java

--- src/main/java/WordCensor.java
+++ src/main/java/WordCensor.java
@@ -5,7 +5,7 @@ public class WordCensor
     public static void main(String[] args)
     {    
         Scanner sc = new Scanner(System.in);
-        System.out.print("Example input: ");
+        System.out.println("Example input: ");
         String word = sc.nextLine();
         
         String word1 = word.replace("oops","***");
