81edf690676bc5dc839eb20f924dfbd7aa57bc5d
Anderscodes1 <anderschasegustafson@icloud.com>
2025-11-11 11:15:34 -0500

    helooooo

--- src/main/java/PalindromeChecker.java
+++ src/main/java/PalindromeChecker.java
@@ -2,6 +2,37 @@ import java.util.Scanner;
 
 public class PalindromeChecker {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a word: ");
+        String word = sc.nextLine();
+
+        int start = 0;
+        int end = word.length() - 1;
+        boolean palindrome = false;
+
+        for (int i=0; i<word.length(); i++)
+        {
+            if (word.charAt(i) == word.charAt(word.length() - i - 1))
+            {
+                palindrome = true;
+            }
+            else {
+                palindrome = false;
+            }
+        }
+
+        if (palindrome)
+        {
+            System.out.print(word + " is a palindrome!");
+        }
+        else {
+            System.out.print(word + " is NOT a palindrome.");
+        }
+
+
+
+
+
         // TODO: Create a Scanner and read a full line from the user with nextLine().
         //       Keep the original input string so you can print it in the final message.
 
